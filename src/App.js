35f425700eca1ef38aef84b6451ec30e40cd8988
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-12-04 13:11:28 +0530

    done with navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+
+function App() {
+  return (
+    <>
+      <PreNavbar />
+      <div>Hi I'm react</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/readme.md
@@ -0,0 +1,2 @@
+in react  we can use plain javascript above the react functional ccomponents
+And that variables of plain js can access using curly brackets in react functional compoenent.
