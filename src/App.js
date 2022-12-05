eb7f05a1826ff63fd2ddf585f498011d36f48f45
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-12-05 10:40:19 +0530

    done with offers section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import PreNavbar from "./components/PreNavbar";
-import Slider from './components/Slider'
+import Slider from "./components/Slider";
 import { default as data } from "./data/data.json";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Offers } from "./components/Offers";
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
-      <Slider start={data.banner.start}/>
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer}/>
     </Router>
   );
 }
---
+++ src/components/offer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const offer = ({ src, link, index }) => {
+  return (
+    <div>
+      <a href={link}>
+        <img src={src} alt={`${index} offer`} />
+      </a>
+    </div>
+  );
+};
+
+export default offer;
