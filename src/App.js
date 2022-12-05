21f995e6ec2f9238d75e4e6bd612b46315980035
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-12-05 10:19:44 +0530

    done with curousel by react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import PreNavbar from "./components/PreNavbar";
+import Slider from './components/Slider'
+import { default as data } from "./data/data.json";
 import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start}/>
     </Router>
   );
 }
