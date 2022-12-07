8383e6b9baf0c171e576978a1c72e23fc401e602
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-12-07 10:25:08 +0530

    Done with heading, accessorries mneu and star products.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import PreNavbar from "./components/PreNavbar";
+import { Offers } from "./components/Offers";
+import Heading from "./components/Heading";
 import Slider from "./components/Slider";
+import HotAccessoriesMenu from './components/HotAccessoriesMenu'
 import { default as data } from "./data/data.json";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Offers } from "./components/Offers";
+import StarProduct from "./components/StarProduct";
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
       <Slider start={data.banner.start} />
-      <Offers offer={data.offer}/>
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct}/>
+      <Heading text = "HOT ACCESSORIES "/>
+      <HotAccessoriesMenu/>
     </Router>
   );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-lone-blocks */
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+// map function returns a new array 
+{/*
+1] map is use for traversing an array
+2] map is use for traversing an array using key
+*/ }
 const Slider = ({ start }) => {
   return (
     <Carousel fade>
